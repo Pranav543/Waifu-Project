02fbdbb4503a96c442e38230a032527ea12a5c22
Ska2SH <pranavnaik543@gmail.com>
2021-02-28 19:16:47 +0530

    slight changes

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -13,39 +13,25 @@ export function Modal() {
     // code below
     const web3 = await getWeb3();
     const accounts = await web3.eth.getAccounts();
-<<<<<<< HEAD
-    console.log(accounts)
-      const networkId = await web3.eth.net.getId();
-      console.log(networkId)
-      const address = `0x3618a00e2aDd9A924D1cA2C623F065599573FAD6`;
-      console.log(address)
-      const instance = new web3.eth.Contract(CreateWaifu.abi, address);
-      const tx = await instance.methods.requestNewRandomWaifu(7887, name, origin, description).send({
-        from: accounts[0].toLowerCase()
-      })
-      console.log("Tx Hash: ", tx)
-      const num = await instance.methods.getNumberOfWaifus().call();
-      console.log("Number Of Waifu: ", num)
-      const second_waifu = await instance.methods.waifus(0).call();
-      console.log("Second Waifu: ", second_waifu)
-=======
     console.log(accounts);
     const networkId = await web3.eth.net.getId();
-    console.log(networkId);
-    const address = `0xaa608FE67f3BAb163d843c67798Bd45B2949eAF0`;
+    const randomNumber = Math.floor(Math.random() * 100000);
+    // const imgSource = `https://www.thiswaifudoesnotexist.net/example-${randomNumber}.jpg`;
+    const address = `0x7Fc753337F15F47cf7BCE74545c2753E3dA12bda`;
     console.log(address);
     const instance = new web3.eth.Contract(CreateWaifu.abi, address);
-    const tx = await instance.methods
-      .requestNewRandomWaifu(7887, name, origin, description)
-      .send({
-        from: accounts[0].toLowerCase(),
-      });
-    console.log("Tx Hash: ", tx);
-    const num = await instance.methods.getNumberOfWaifus().call();
+    // const tx = await instance.methods
+    //   .requestNewRandomWaifu(7887, name, origin, description, "imgSource")
+    //   .send({
+    //     gas:300000,
+    //     from: accounts[0].toLowerCase(),
+        
+    //   });
+    // console.log("Tx Hash Is : ", tx);
+    const num = await instance.methods.getWaifusCount(accounts[0]).call();
     console.log("Number Of Waifu: ", num);
-    const second_waifu = await instance.methods.waifus(1).call();
-    console.log("Second Waifu: ", second_waifu);
->>>>>>> 9a14ac72d205cf5804e8325ce4cc907ca1cbaa8d
+    // const second_waifu = await instance.methods.waifus(0).call();
+    // console.log("Second Waifu: ", second_waifu);
   };
 
   return (
